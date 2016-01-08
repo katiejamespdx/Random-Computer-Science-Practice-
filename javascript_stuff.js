d67724dabef57abcb3450e691f02c81f12eeddd2
Katie James <katie162@stmaryspdx.net>
2016-01-08 10:29:37 -0800

    This is my first push to Github

---
+++ javascript_stuff.js
@@ -0,0 +1,11 @@
+// javascript stuff goes here 
+console.log ("PLEASE WORK PROPERLY!!")
+
+ <script type="text/javascript">
+        function insert(){
+            var src = document.getElementById("gamediv");
+            var img = document.createElement("img");
+            img.src = "img/eqp/"+this.apparel+"/"+this.facing+"_idle.png";
+            src.(img);
+        }
+     </script>
